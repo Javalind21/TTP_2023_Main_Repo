eacd2651b8b43dc59f03242a931b82cac8122c5e
Javalind21 <javaughnlindsay21@gmail.com>
2023-07-27 10:26:29 -0400

    Lab

---
+++ react-starwars/src/StarWarsCharacters.js
@@ -0,0 +1,20 @@
+// import React, { useEffect } from "react";
+// const [characters, setCharacters] = useState([]);
+
+// function StarWarsCharacters() {
+//     useEffect(() => {
+//         const fetchCharacters = async () => {
+//           try {
+//             const response = await axios.get("https://swapi.dev/api/people/");
+//             console.log(response.data.results);
+//           } catch (error) {
+//             console.error("Error fetching data", error);
+//           }
+//         };
+      
+//         fetchCharacters();
+//       }, []);
+// }
+
+
+// export default StarWarsCharacters;
