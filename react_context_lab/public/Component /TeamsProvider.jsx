81d15a728ae4b3b394656f2fca05d450c519d105
Javalind21 <javaughnlindsay21@gmail.com>
2023-08-02 17:51:46 -0400

    TTP_2023

---
+++ react_context_lab/public/Component /TeamsProvider.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import TeamContext from "./TeamContext";
+
+const TeamsProvider = ({ children }) => {
+  const [team, setTeam] = useState(null);
+  const [allTeams, setAllTeams] = useState([]);
+
+  // Rest of the code goes here...
+  useEffect(() => {
+    // Replace this with your actual API call
+    const fetchTeams = async () => {
+      const teams = ["Team 1", "Team 2", "Team 3"];
+      setAllTeams(teams);
+    };
+
+    fetchTeams();
+  }, []);
+
+
+  return (
+    <TeamContext.Provider value={{ team, setTeam, allTeams, setAllTeams }}>
+      {children}
+    </TeamContext.Provider>
+  );
+};
+
+export default TeamsProvider;
