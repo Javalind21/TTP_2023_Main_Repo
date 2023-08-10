3e967c7caeb0499cf26071c2b111d22fc3160b94
Javalind21 <javaughnlindsay21@gmail.com>
2023-08-10 13:50:17 -0400

    TTP 2023

--- Project_6-wizarding-schools-network/server/db/index.js
+++ Project_6-wizarding-schools-network/server/db/index.js
@@ -1,8 +1,70 @@
 "use strict";
 
-const db = require("./db");
+const { db, Sequelize } = require("./db");
 
 // Require your models and make your associations
+const school = db.define("School", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: sequelize.STRING,
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.DATE,
+  },
+  
+});
+
+const student = db.define("Student", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  team: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
 
 module.exports = {
   db,
